e6f6c43f0a26d33a3fd45476801e766fd7aeebf7
Roy Gero <roy.gero.ii@gmail.com>
2025-01-06 21:33:53 -0500

    Adding the stub of the Landing Page.

--- AchievementTracker/src/screens/LandingScreen.tsx
+++ AchievementTracker/src/screens/LandingScreen.tsx
@@ -1,21 +1,37 @@
-import { StyleSheet, View } from "react-native"
-import { Text, useTheme } from "react-native-paper"
+import { Button, Divider, Surface, Text, useTheme } from "react-native-paper"
+import { ImageBackground, StyleSheet } from "react-native"
 
 const LandingScreen = () => {
   const theme = useTheme();
   return (
-    <View style={[styles.container, { backgroundColor: theme.colors.background }]} >
-      <Text>Dumb Landing Page</Text>
-    </View>
+    <ImageBackground 
+      source={require('../../assets/background.jpg')} 
+      style={styles.background}
+    >
+      <Surface style={styles.container} elevation={3}>
+        <Text variant="titleLarge" style={{fontWeight: 'bold'}}>The Achievement Tracker</Text>
+        <Divider/>
+        <Text variant="bodyMedium">Welcome to the Achievement Tracker. This website is meant to track your personal achievements</Text>
+        <Button mode="contained" style={{marginTop: 20}} onPress={() => console.log('Pressed')}>Login with Google</Button>
+      </Surface>
+    </ImageBackground>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    width: '80%',
+    height: "30%",
+    borderRadius: 5,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 
